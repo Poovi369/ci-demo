2c7832dd478a0f98ab0d1a8fa6b829d78d5087b5
Poovitha <poovivelu3@gmail.com>
2025-11-25 17:11:34 +0530

    added health status

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Hello from CI/CD demo!.. 🎉');
 });
+app.get('/health', (req, res) => res.json({ ok: true }));
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
